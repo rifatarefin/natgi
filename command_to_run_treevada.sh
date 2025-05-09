ae21b2d08b0c012aa3b0137efbbc7f0980f501cc
Rahman Shanto (sr53282) <sr53282@austin.utexas.edu@ece-a58482.austin.utexas.edu>
2025-05-08 22:39:04 -0500

    script to run all grammar

--- command_to_run_treevada.sh
+++ command_to_run_treevada.sh
@@ -10,21 +10,29 @@ seed="r1"
 if [[ $1 == "json" || $1 == "lisp" || $1 == "while"  || $1 == "xml" || $1 == "turtle"  ]]; then
     bench="micro-benchmarks/$1"
 
-echo "**** Running search.py ****"
-echo "python3 search.py external Seed_Programs/$bench/parse_$1 Seed_Programs/$bench/guides-r1 Seed_Programs/$1-$seed-tv.log"
-python3 search.py external Seed_Programs/$bench/parse_$1 Seed_Programs/$bench/guides-r1 Seed_Programs/$1-$seed-tv.log
+    echo "**** Running search.py ****"
+    echo "python3 search.py external Seed_Programs/$bench/parse_$1 Seed_Programs/$bench/guides-r1 Seed_Programs/$1-$seed-tv.log"
+    python3 search.py external Seed_Programs/$bench/parse_$1 Seed_Programs/$bench/guides-r1 Seed_Programs/$1-$seed-tv.log
 
-echo "**** Running eval.py ****"
+    echo "**** Running eval.py ****"
 python3 eval.py external Seed_Programs/$bench/parse_$1 Seed_Programs/$bench/test_set Seed_Programs/$1-$seed-tv.log #json, lisp, while, xml, turtle
 
 elif  [[ $1 == "c-500" ]]; then
     bench="tinyc"
     seed="r5"
     #$1="tinyc"
-echo "**** Running search.py ****"
-python3 search.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/tinyc-train-r5 Seed_Programs/$1-$seed-tv.log
-echo "**** Running eval.py ****"
-python3 eval.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/tinyc-test Seed_Programs/$1-$seed-tv.log #json, lisp, while, xml, turtle
+    echo "**** Running search.py ****"
+    python3 search.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/tinyc-train-r5 Seed_Programs/$1-$seed-tv.log
+    echo "**** Running eval.py ****"
+    python3 eval.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/tinyc-test Seed_Programs/$1-$seed-tv.log
+
+elif [[ $1 == "minic" ]]; then
+    bench="$1"
+    echo "python3 search.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/$bench-train-r0 Seed_Programs/$1-$seed-tv.log"
+    python3 search.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/$bench-train-r0 Seed_Programs/$1-$seed-tv.log
+    echo "**** Running eval.py ****"
+    python3 eval.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/$bench-test Seed_Programs/$1-$seed-tv.log
+
 elif [[ $1 == "curl" || $1 == "tinyc" ]]; then
     bench="$1"
     echo "python3 search.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/$bench-train-r1 Seed_Programs/$1-$seed-tv.log"
@@ -40,20 +48,3 @@ elif [[ $1 == "lua" || $1 == "mysql" || $1 == "c" || $1 == "tiny" ]]; then
     python3 eval.py external glade-2/$bench/parse_$bench glade-2/$bench/$bench-test glade-2/$1-$seed-tv.log
 fi
 echo "**** Running search.py ****"
-#echo "python3 search.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/$bench-train-$seed Seed_Programs/$bench-$seed-tv.log"
-#python3 search.py external Seed_Programs/micro-benchmarks/json/parse_micro-benchmarks/json Seed_Programs/micro-benchmarks/json/micro-benchmarks/json-train-r1 Seed_Programs/micro-benchmarks/json-r1-tv.log
-
-#echo "**** Running eval.py ****"
-#echo "python3 eval.py external Seed_Programs/$bench/parse_$bench Seed_Programs/$bench/$bench-test Seed_Programs/$bench-$seed-tv.log"
-
-exit
-
-
-#python3 search.py external Seed_Programs/micro-benchmarks/while/parse_while Seed_Programs/micro-benchmarks/while/guides-r1/ Seed_Programs/micro-benchmarks/while/while.log
-#python3 eval.py external Seed_Programs/micro-benchmarks/while/parse_while Seed_Programs/micro-benchmarks/while/guides-r1/ 
-#Seed_Programs/micro-benchmarks/while/parse_while Seed_Programs/micro-benchmarks/while/guides-r1/ Seed_Programs/micro-benchmarks/while/while.log
-
-
-
-#python3 eval.py external Seed_Programs/tinyc/parse_tinyc Seed_Programs/tinyc/tinyc-train-r1  Seed_Programs/tinyc/tinyc-train-r1/tinyc-train-r1.log
-
