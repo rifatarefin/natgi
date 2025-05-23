b79ecc53dffe25fd3e72aa71249aafbbcf381d1d
SHANTO <shanto.rahman@utexas.edu>
2025-05-23 10:32:54 -0500

    command to run all programs

--- tmp.sh
+++ tmp.sh
@@ -1,4 +1,12 @@
-#bash command_to_run_treevada.sh "json"
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "json"; done >  logs_treevada_false_llm_true/json.log
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "lisp"; done >  logs_treevada_false_llm_true/lisp.log
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "while"; done >  logs_treevada_false_llm_true/while.log
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "xml"; done >  logs_treevada_false_llm_true/xml.log
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "turtle"; done >  logs_treevada_false_llm_true/turtle.log
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "c-500"; done >  logs_treevada_false_llm_true/c-500.log
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "curl"; done >  logs_treevada_false_llm_true/curl.log
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "tinyc"; done >  logs_treevada_false_llm_true/tinyc.log
+for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "minic"; done >  logs_treevada_false_llm_true/minic.log
 #bash command_to_run_treevada.sh "lisp"
 #bash command_to_run_treevada.sh "while"
 #bash command_to_run_treevada.sh "xml"
@@ -13,4 +21,5 @@
 for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "c"; done > shanto_log_c.log
 #for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "tiny"; done > shanto_log_tiny.log
 #for i in {1..5}; do timeout 12h bash command_to_run_treevada.sh "lua"; done > shanto_log_lua.log
-#bash parse_result.sh "shanto_log_tiny.log"
+
+#bash parse_result.sh "logs_treevada_false_llm_true/shanto_log_tiny.log"
