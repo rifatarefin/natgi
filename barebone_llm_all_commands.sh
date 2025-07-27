515654a393d4cf36af21b3997be8fac4a695ea78
Rahman Shanto (sr53282) <sr53282@austin.utexas.edu@ece-a58482.austin.utexas.edu>
2025-07-26 20:07:26 -0500

    script to run all command for barebone

--- barebone_llm_all_commands.sh
+++ barebone_llm_all_commands.sh
@@ -1,15 +1,15 @@
-#for i in {1..50}; do bash runAll_for_gpt.sh  tinyc-r1-$i; done |& tee log-tinyc
-#for i in {1..50}; do bash runAll_for_gpt.sh  curl-r1-$i; done |& tee log-curl
-#for i in {1..50}; do bash runAll_for_gpt.sh  microbench-json-r1-$i; done |& tee log-json
-#for i in {1..50}; do bash runAll_for_gpt.sh  microbench-lisp-r1-$i; done |& tee log-lisp
-#for i in {1..50}; do bash runAll_for_gpt.sh  microbench-turtle-r1-$i; done |& tee log-turtle
-#for i in {1..50}; do bash runAll_for_gpt.sh  microbench-while-r1-$i; done |& tee log-while
-#for i in {1..50}; do bash runAll_for_gpt.sh  microbench-xml-r1-$i; done |& tee log-xml
-#for i in {1..50}; do bash runAll_for_gpt.sh  microbench-c-500-r1-$i; done |& tee log-microbench-c-500
-#for i in {1..50}; do bash runAll_for_gpt.sh  minic-r1-$i; done |& tee log-minc
+for i in {1..1}; do bash runAll_for_gpt.sh  tinyc-r1-$i; done |& tee barebone_llm/tinyc.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  curl-r1-$i; done |& tee barebone_llm/curl.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  microbench-json-r1-$i; done |& tee barebone_llm/json.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  microbench-lisp-r1-$i; done |& tee barebone_llm/lisp.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  microbench-turtle-r1-$i; done |& tee barebone_llm/turtle.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  microbench-while-r1-$i; done |& tee barebone_llm/while.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  microbench-xml-r1-$i; done |& tee barebone_llm/xml.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  microbench-c-500-r1-$i; done |& tee barebone_llm/c-500.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  minic-r1-$i; done |& tee barebone_llm/minic.log
 #
 #
-#for i in {1..50}; do bash runAll_for_gpt.sh  glade2-lua-$i; done |& tee log-glade2-lua
-for i in {1..50}; do bash runAll_for_gpt.sh  glade2-mysql-$i; done |& tee log-glade2-mysql
-for i in {1..50}; do bash runAll_for_gpt.sh  glade2-c-$i; done |& tee log-glade2-c
-for i in {1..50}; do bash runAll_for_gpt.sh  glade2-tiny-$i; done |& tee log-glade2-tiny
+#for i in {1..10}; do bash runAll_for_gpt.sh  glade2-lua-$i; done |& tee barebone_llm/glade2-lua.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  glade2-mysql-$i; done |& tee barebone_llm/glade2-mysql.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  glade2-c-$i; done |& tee barebone_llm/glade2-c.log
+#for i in {1..10}; do bash runAll_for_gpt.sh  glade2-tiny-$i; done |& tee barebone_llm/glade2-tiny.log
